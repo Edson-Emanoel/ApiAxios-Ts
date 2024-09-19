3e787a6b06b8b0161c9af5de873d764bf3eb3207
Edson-Emanoel <156195854+Edson-Emanoel@users.noreply.github.com>
2024-09-19 09:27:43 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import axios from 'axios';
 import './App.css'
-import axios from 'axios'
 import { FormEvent, useEffect, useState } from 'react'
+import { Bounce, toast } from 'react-toastify';
 
 interface ICategoria {
   id: string;  
@@ -10,15 +11,16 @@ interface ICategoria {
 
 function App() {
   const url = "http://localhost:3000/categoria"
-
+  
   const [id, setId] = useState("");
   const [nome, setNome] = useState("");
   const [desc, setDesc] = useState("");
+  
+  const [data, setData] = useState([]);
+  const [classInserir, setClassInserir] = useState("");
+  const [classEditar, setClassEditar] = useState("sumir");
 
-  const [classInserir, setClassInserir] = useState("")
-  const [classEditar, setClassEditar] = useState("sumir")
-  const [data, setData] = useState([])
-
+  // Listar
   useEffect(() => {
     axios.get(url)
       .then(res => setData(res.data))
@@ -31,47 +33,88 @@ function App() {
     })
   }
 
-  const Cadastrar = (event: FormEvent) => {
+  /* Valida os Campos */const Cadastrar = (event: FormEvent) => {
     event.preventDefault();
 
     if (nome === "") {
-      alert("Insira um nome da Categoria!!")
+      alert("Insira um Nome da Categoria!")
+      return true;
     } else if (desc === "") {
-      alert("Insira uma descrição da Categoria!!")
+      alert("Insira uma Descrição da Categoria!")
+      return true;
     } else {
-      alert("Cadastrando...")
+      toast.success('O Cadastro foi realizado com Sucesso!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        transition: Bounce
+      })
+
+      Inserir();
     }
 
-    Inserir();
+    setNome("");
+    setDesc("");
   }
-  
-  const Remover = (id: string) => {
+
+  const Remover = (id: string, nome: string) => {
     // alert("Deletando no id: " + id)
 
-    setId(id)
+    const confirm = window.confirm('Deseja realmente apagar  ? ' + nome )
 
-    axios.delete(url + "/" + id)
+    if(confirm){  
+      setId(id)
+
+      axios.delete(url + "/" + id)
+      toast.success('A Categoria foi Apagada com Sucesso!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+      });
+    }
+
   }
 
   const Carregar = (id: string, nome: string, desc: string) => {
-    // alert("id: " + id + " nome: " + nome + " Desc: " + desc)
-    
-    setId(id)
-    setNome(nome)
-    setDesc(desc)
+    // alert("Id: " + id + " Nome: " + nome + " Descrição: " + desc);
 
-    setClassEditar("")
-    setClassInserir("sumir")
+    setId(id);
+    setNome(nome);
+    setDesc(desc);
+
+    setClassEditar("");
+    setClassInserir("sumir");
   }
 
   const Editar = (event: FormEvent) => {
     event.preventDefault();
+
+    // console.log("Nome: " + nome + " Desc: " + desc);
 
     axios.put(url + "/" + id, {
       nome,
       desc
     })
     .then(() => {
+      toast.success('A Categoria foi Editada com Sucesso!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+      });
+
       setId("")
       setNome("")
       setDesc("")
@@ -82,26 +125,26 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Cadastro de Categorias</h1>
+    <div className="container">      
+      <h2>Cadastro de Categorias</h2>
 
-      <form className='mt-5 mb-4'>
+      <form className="mt-5 mb-4">
         <div className="row mb-3">
           <div className="col">
             <input
               type="text"
               placeholder='Nome da Categoria'
-              className="form-control"
+              className='form-control'
               value={nome}
               onChange={(e) => setNome(e.target.value)}
             />
           </div>
-
+          
           <div className="col">
             <input
               type="text"
-              placeholder='Descricao da Categoria'
-              className="form-control"
+              placeholder='Descrição da Categoria'
+              className='form-control'
               value={desc}
               onChange={(e) => setDesc(e.target.value)}
             />
@@ -114,6 +157,7 @@ function App() {
         </div>
       </form>
 
+      <h2 className='mb-4'>Lista de Categorias</h2>
       <table className="table table">
         <thead>
           <tr>
@@ -129,11 +173,12 @@ function App() {
               <td>{item.id}</td>
               <td>{item.nome}</td>
               <td className='mx-2'>{item.desc}</td>
-              <td className='mx-2'>
-                <button className='btn btn-warning mx-2' onClick={() => Carregar(item.id, item.nome, item.desc)}>
+              <td className="mx-2">
+                <button className="btn btn-warning mx-2" onClick={() => Carregar(item.id, item.nome, item.desc)}>
                   <i className="fa-solid fa-pen-to-square"></i>
                 </button>
-                <button className='btn btn-danger' onClick={() => Remover(item.id)}>
+
+                <button className="btn btn-danger" onClick={() => Remover(item.id, item.nome)}>
                   <i className="fa-solid fa-trash"></i>
                 </button>
               </td>
